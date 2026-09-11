bac466aa7ff9d885b03ab40a121a9b4810d3734d
Mgteixeira2112 <102971119+Mgteixeira2112@users.noreply.github.com>
2026-09-10 21:05:29 -0300

    feat: carregar estilos do template Boutique

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import './public-site-boutique.css';
 import { api } from './services/api.ts';
 import {
   loadSettingsCloud,
